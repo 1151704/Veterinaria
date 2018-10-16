fd1f479b0dcb20a73bf3a8b205a5e399615e00fc
Omar Montes <omarmontes.879@gmail.com>
2018-10-16 10:05:08 -0500

    .sql de base de datos

--- Veterinaria/db/veterinaria.sql
+++ Veterinaria/db/veterinaria.sql
@@ -19,6 +19,9 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `veterinaria`
 --
+drop database if exists poo2_veterinaria;
+create database poo2_veterinaria;
+use poo2_veterinaria;
 
 -- --------------------------------------------------------
 
